7571ca612300c1b8554bac879dfbccd25c96910f
Gong LiangYun <18525499+liangyungong@users.noreply.github.com>
2022-10-21 20:28:19 +0800

    [fix]

--- hw02/hw02.py
+++ hw02/hw02.py
@@ -31,7 +31,8 @@ def product(n, term):
     >>> product(3, triple)    # 1*3 * 2*3 * 3*3
     162
     """
-    "*** YOUR CODE HERE ***"
+    from functools import reduce
+    return reduce(mul, [term(i) for i in range(1,n+1)])
 
 
 def accumulate(merger, start, n, term):
@@ -58,7 +59,9 @@ def accumulate(merger, start, n, term):
     >>> accumulate(lambda x, y: (x + y) % 17, 19, 20, square)
     16
     """
-    "*** YOUR CODE HERE ***"
+    seq = [start,]+[term(i) for i in range(1,n+1)]
+    from functools import reduce
+    return reduce(merger, seq)
 
 
 def summation_using_accumulate(n, term):
@@ -75,7 +78,7 @@ def summation_using_accumulate(n, term):
     >>> [type(x).__name__ for x in ast.parse(inspect.getsource(summation_using_accumulate)).body[0].body]
     ['Expr', 'Return']
     """
-    "*** YOUR CODE HERE ***"
+    return accumulate(add,0,n,term)
 
 
 def product_using_accumulate(n, term):
@@ -92,4 +95,4 @@ def product_using_accumulate(n, term):
     >>> [type(x).__name__ for x in ast.parse(inspect.getsource(product_using_accumulate)).body[0].body]
     ['Expr', 'Return']
     """
-    "*** YOUR CODE HERE ***"
+    return accumulate(mul,1,n,term)
